7aa56c9b239c217aba6f6640f1047f59d935c4ba
Chisui <1214647657@qq.com>
2026-03-10 11:30:33 +0800

    fix: 使用hash路由，解决刷新404问题

--- apps/personal-homepage/src/router/index.ts
+++ apps/personal-homepage/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Projects from '../views/Projects.vue'
@@ -13,7 +13,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL),
+  history: createWebHashHistory(import.meta.env.VITE_BASE_URL),
   routes
 })
 
